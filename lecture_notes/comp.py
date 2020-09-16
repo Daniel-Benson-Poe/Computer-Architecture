5501e070449771f30ddd0424008c64a44b719e8e
Poenomaly <djbenson741@gmail.com>
2020-09-16 12:05:06 -0600

    Added lecture notes for day 3.

--- lecture_notes/comp.py
+++ lecture_notes/comp.py
@@ -1,3 +1,13 @@
+import sys
+
+"""
+Get the file path from the command line
+Sanitize the data from that file
+    Ignore blank lines, whitespace, comments
+    Splitting the input file per line
+    Turn into program instructions (str->int)
+"""
+
 # These all mean the same thing:
 #### Index into the memory array
 #### Address
@@ -6,15 +16,36 @@
 
 ## operands == arguments to the instruction
 
-memory = [
-    1,  # print Beej  address 0
-    3,  # save_reg R1, 37
-    1,
-    37,
-    4,  # print reg
-    1,  # R1
-    2,  # halt
-]
+memory = [0] * 256
+
+if len(sys.argv) != 2:
+    print("usage: comp.py filename")
+    sys.exit(1)
+
+try:
+    address = 0
+
+    with open(sys.argv[1]) as f:
+        for line in f:
+            t = line.split('#')
+            n = t[0].strip()
+
+            if n == '':
+                continue
+            
+            try:
+                n = int(n)
+            except ValueError:
+                print(f"Invalid number '{n}'")
+                sys.exit()
+            
+            memory[address] = n
+            address += 1
+
+except FileNotFoundError:
+    print(f"File not found: {sys.argv[1]}")
+    sys.exit(2)
+
 
 registers = [0] * 8  # R0-R7
 
@@ -24,6 +55,18 @@ registers = [0] * 8  # R0-R7
 # R0, R1, R2, ..., R6, R7
 
 pc = 0  # "Program Counter": address of the currently-executing instruction
+
+SP = 7
+
+SAVE_REG = 3
+PRINT_REG = 4
+PRINT_BEEJ = 1
+PUSH = 5
+POP = 6
+HALT = 2
+
+
+registers[SP] = 0xF4  # Init SP
 
 running = True
 
@@ -40,25 +83,77 @@ running = True
 while running:
     ir = memory[pc]  # "Instruction Register": holds copy of the currently-executing instruction
 
-    if ir == 1:
+    if ir == PRINT_BEEJ:
         print("Beej!")
         pc += 1
     
-    elif ir == 2:
+    elif ir == HALT:
         running = False
 
-    elif ir == 3:  # save reg
+    elif ir == SAVE_REG:  # save reg
         reg_num = memory[pc + 1]
         value = memory[pc + 2]
         registers[reg_num] = value
         print(registers)
         pc += 3  # must allows increment pc by total operands -- in this case, 3
 
-    elif ir == 4:  # print reg
+    elif ir == PRINT_REG:  # print reg
         reg_num = memory[pc + 1]
         print(registers[reg_num])
         pc += 2
 
+    elif ir == PUSH:  # PUSH
+        # Decrement SP
+        registers[SP] -= 1
+
+        # Get reg num
+        reg_num = memory[pc + 1]
+
+        # get value to push
+        value = registers[reg_num]
+
+        # copy value to SP
+        top_of_stack_addr = registers[SP]
+        memory[top_of_stack_addr] = value
+
+        pc += 2
+
+    elif ir == POP:   # POP
+        # get reg to pop into
+        reg_num = memory[pc + 1]
+
+        # get top of stack addr
+        top_top_of_stack_addr = registers[SP]
+
+        # get value at top of stack
+        value = memory[top_of_stack_addr]
+
+        # store value in register
+        registers[reg_num] = value
+
+        # increment stack pointer
+        registers[SP] += 1
+        pc += 2
+
+    # PUSH:
+    # 1. Decrement SP
+    # 2. Copy the register value into SP's location
+
+    # POP:
+    # 1. Copy value from SP's location to the reg
+    # 2. Incrememnt SP
+
     else:
         print(f"Unknown instruction {ir}")
-        pc += 1
+        sys.exit(3)
+        # pc += 1
+
+    """
+    # For the LS-8 to move the PC
+
+    number_of_operands = (irt & 0b11000000) >> 6
+
+    how_far_to_move_pc = number_of_operands + 1
+
+    pc += how_far_to_move_pc
+    """
